62096461207612fb904b3fd544336e5e45e8af89
chris <chris@192.168.1.117>
2014-07-04 13:58:52 +0200

    onClick auf inventoryList

--- inventoryApp/src/com/example/inventoryapp/InventoryListActivity.java
+++ inventoryApp/src/com/example/inventoryapp/InventoryListActivity.java
@@ -8,39 +8,43 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.inventoryapp.model.CommodityModel;
 import com.example.inventoryapp.model.PersonModel;
 
 public class InventoryListActivity extends RoboActivity {
-	
-	
-	@InjectView(R.id.commodityList) private ListView commoditiesView;
-	
-	List<CommodityModel> commodities ;
+
+	@InjectView(R.id.commodityList)
+	private ListView commoditiesView;
+
+	List<CommodityModel> commodities;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.inventory_list_main);
-		
-		commodities = PersonModel.getPersonInstance().getInventaryList().get(0).getInventoryCommodities();
+
+		commodities = PersonModel.getPersonInstance().getInventaryList().get(0)
+				.getInventoryCommodities();
 
 		commoditiesView.setAdapter(new InventoryListAdapter());
-
-	}
-	
-	public class InventoryListAdapter extends BaseAdapter {
 		
+		
+	}
+
+	class InventoryListAdapter extends BaseAdapter {
+
 		private LayoutInflater inflater;
-		 
-	    public InventoryListAdapter() {
-	        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	    }
+
+		public InventoryListAdapter() {
+			inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
 
 		@Override
 		public int getCount() {
@@ -61,20 +65,29 @@ public class InventoryListActivity extends RoboActivity {
 		@Override
 		public View getView(int index, View recycledRowView, ViewGroup arg2) {
 			View rowView = recycledRowView;
-			if (rowView == null){
+			if (rowView == null) {
 				rowView = inflater.inflate(R.layout.inventary_list_item, arg2,
-							false);
+						false);
 			}
-			
+
 			CommodityModel commodityModel = commodities.get(index);
-			
+
 			TextView idView = (TextView) rowView.findViewById(R.id.commodityId);
-			TextView titleView = (TextView) rowView.findViewById(R.id.commodityTitle);
-			TextView priceView = (TextView) rowView.findViewById(R.id.commodityPrice);
-	        idView.setText("" + commodityModel.getCommodityId());
-	        titleView.setText(commodityModel.getCommodityTitle());
-	        priceView.setText("" + commodityModel.getCommodityPrice());
-	        
+			TextView titleView = (TextView) rowView
+					.findViewById(R.id.commodityTitle);
+			TextView priceView = (TextView) rowView
+					.findViewById(R.id.commodityPrice);
+			idView.setText("" + commodityModel.getCommodityId());
+			titleView.setText(commodityModel.getCommodityTitle());
+			priceView.setText("" + commodityModel.getCommodityPrice());
+			
+			rowView.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					Toast.makeText(getApplicationContext(), "Hallo", Toast.LENGTH_SHORT).show();
+				}});
+
 			return rowView;
 		}
 	}
