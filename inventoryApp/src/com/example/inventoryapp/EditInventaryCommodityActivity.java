a7505b4591156b8355aa81a0b0d76e36540a5c6c
DanielStettler <stettler.daniel@gmail.com>
2014-07-04 14:22:37 +0200

    Neuer Stand EditInventaryCommodityActivity

--- inventoryApp/src/com/example/inventoryapp/EditInventaryCommodityActivity.java
+++ inventoryApp/src/com/example/inventoryapp/EditInventaryCommodityActivity.java
@@ -25,19 +25,39 @@ public class EditInventaryCommodityActivity extends RoboActivity {
 //	}
 
 	@InjectView(R.id.textTitel) private TextView inventaryPrices;
-	@InjectView(R.id.labelPrice) private TextView inventaryCommodityprice;
-	@InjectView(R.id.labelType) private TextView inventaryCommodityType;
+	@InjectView(R.id.textPrice) private TextView inventaryCommodityprice;
+	@InjectView(R.id.textType) private TextView inventaryCommodityType;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_inventory_main);
-		
-		inventaryPrices.setTag("54'000 CHF / 140'000 CHF");
-		inventaryCommodityprice.setText("140 CHF");
-		
+				
 		setcList(new PersonLoader().getPerson(this).getSingleInventary().getInventoryCommodities());
-		
+		setSelectedCommodity (getFirstCommodity());
+		inventaryPrices.setText(getInventaryPricesText());
+		inventaryCommodityprice.setText(((Integer) getSelectedCommodity().getCommodityPrice()).toString());
+		inventaryCommodityType.setText("Möbel");
+	}
+
+	private CharSequence getInventaryPricesText() {
+		StringBuilder b = new StringBuilder();
+		Integer summe = 0;
+		for (CommodityModel c : cList) {
+			summe = summe + c.getCommodityPrice();
+		}
+		b.append("47000");
+		b.append(" CHF / ");
+		b.append(summe.toString());
+		b.append(" CHF");
+		return b.toString();
+	}
+
+	private CommodityModel getFirstCommodity() {
+		for ( CommodityModel c : cList) {
+			return c;
+		}
+		return null;
 	}
 
 	public List<CommodityModel> getcList() {
